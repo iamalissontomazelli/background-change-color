85a1f60fc89ffcac27e884b427c6aa38f682348f
alissonalvez@hotmail.com <alissonalvez@hotmail.com>
2023-09-23 07:09:00 +0100

    add files on github

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("btn-laranja").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class","laranja");
+
+})
+
+document.getElementById("btn-vermelho").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class","vermelho");
+
+})
+
+document.getElementById("btn-porpura").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class","porpura");
+
+})
